a7d127cefd2ad2eae590cad0f3f61f2454cacfaf
vivekgoswami934 <vivek@rhombuz.io>
2023-10-21 10:41:38 +0530

    table added with filteration pagination and sorting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
+import { useMemo } from "react";
 import BasicTable from "./Components/BasicTable";
+import { mockData } from "./Components/utils/mock-data";
+import { DateTime } from "luxon";
 
 function App() {
+  const data = useMemo(() => mockData, []);
+
+  const columns = [
+    {
+      header: "ID",
+      accessorKey: "id",
+      footer: "ID",
+    },
+    {
+      header: "NAME",
+      accessorKey: "name",
+      footer: "NAME",
+    },
+    {
+      header: "PLACE",
+      accessorKey: "place",
+      footer: "PLACE",
+    },
+    {
+      header: "DOB",
+      accessorKey: "dob",
+      footer: "DOB",
+      cell: (info) =>
+        DateTime.fromISO(info.getValue()).toLocaleString(
+          DateTime.DATE_MED_WITH_WEEKDAY
+        ),
+    },
+    {
+      header: "CITY",
+      accessorKey: "city",
+      footer: "CITY",
+    },
+    {
+      header: "PHONE",
+      accessorKey: "phone",
+      footer: "PHONE",
+    },
+    {
+      header: "ADDITIONALKEY1",
+      accessorKey: "additionalKey1",
+      footer: "ADDITIONALKEY1",
+    },
+    {
+      header: "ADDITIONALKEY2",
+      accessorKey: "additionalKey2",
+      footer: "ADDITIONALKEY2",
+    },
+  ];
+
   return (
     <div className="App">
       <header>
         <h1> React-TanStack-Table-Components</h1>
       </header>
       <section>
-        <BasicTable />
+        <BasicTable data={data} columns={columns} />
       </section>
     </div>
   );
--- src/Components/BasicTable.jsx
+++ src/Components/BasicTable.jsx
@@ -1,13 +1,16 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { mockData } from "./utils/mock-data";
 import {
   useReactTable,
   flexRender,
-  getCoreRowModel, getPaginationRowModel
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  getFilteredRowModel,
 } from "@tanstack/react-table";
 import { DateTime } from "luxon";
 
-const BasicTable = () => {
+const BasicTable = ({ data, columns }) => {
   // {
   //     id: 1,
   //     name: 'User 1',
@@ -19,73 +22,53 @@ const BasicTable = () => {
   //     additionalKey2: 'Additional Key 2 for User 1',
   //   }
 
-  const data = useMemo(() => mockData, []);
+  const [sorting, setSorting] = useState([]);
+  const [filtering, setFiltering] = useState("");
 
   /** @type import('@tanstack/react-table').ColumnDef<any>*/
-  const columns = [
-    {
-      header: "ID",
-      accessorKey: "id",
-      footer: "ID",
-    },
-    {
-      header: "NAME",
-      accessorKey: "name",
-      footer: "NAME",
-    },
-    {
-      header: "PLACE",
-      accessorKey: "place",
-      footer: "PLACE",
-    },
-    {
-      header: "DOB",
-      accessorKey: "dob",
-      footer: "DOB",
-      cell: (info) =>
-        DateTime.fromISO(info.getValue()).toLocaleString(
-          DateTime.DATE_MED_WITH_WEEKDAY
-        ),
-    },
-    {
-      header: "CITY",
-      accessorKey: "city",
-      footer: "CITY",
-    },
-    {
-      header: "PHONE",
-      accessorKey: "phone",
-      footer: "PHONE",
-    },
-    {
-      header: "ADDITIONALKEY1",
-      accessorKey: "additionalKey1",
-      footer: "ADDITIONALKEY1",
-    },
-    {
-      header: "ADDITIONALKEY2",
-      accessorKey: "additionalKey2",
-      footer: "ADDITIONALKEY2",
-    },
-  ];
+ 
   const table = useReactTable({
     data,
     columns,
-    getCoreRowModel: getCoreRowModel(), getPaginationRowModel : getPaginationRowModel()
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting: sorting,
+      globalFilter: filtering,
+    },
+    onSortingChange: setSorting,
+    onGlobalFilterChange: setFiltering,
   });
 
   return (
     <div className="w3-container">
+      <div>
+        <input
+          type="text"
+          value={filtering}
+          onChange={(e) => setFiltering(e.target.value)}
+        />
+      </div>
       <table className="w3-table-all">
         <thead>
           {table.getHeaderGroups().map((headerGrp) => (
             <tr key={headerGrp.id}>
               {headerGrp.headers.map((header) => (
-                <th key={header.id}>
+                <th
+                  key={header.id}
+                  onClick={header.column.getToggleSortingHandler()}
+                >
                   {flexRender(
                     header.column.columnDef.header,
                     header.getContext()
                   )}
+                  {
+                    { asc: "asc", desc: "desc" }[
+                      header.column.getIsSorted() ?? null
+                    ]
+                  }
                 </th>
               ))}
             </tr>
@@ -109,10 +92,30 @@ const BasicTable = () => {
         </tfoot> */}
       </table>
       <div>
-        <button disabled={!table.getCanPreviousPage()} onClick={()=>table.setPageIndex(0)}>First PAGE</button>
-        <button disabled={!table.getCanPreviousPage()} onClick={()=>table.previousPage() }>PREV PAGE</button>
-        <button disabled={!table.getCanNextPage()} onClick={()=> table.nextPage() }>NEXT PAGE</button>
-        <button disabled={!table.getCanNextPage()} onClick={()=>table.setPageIndex(table.getPageCount() - 1)}>LAST PAGE</button>
+        <button
+          disabled={!table.getCanPreviousPage()}
+          onClick={() => table.setPageIndex(0)}
+        >
+          First PAGE
+        </button>
+        <button
+          disabled={!table.getCanPreviousPage()}
+          onClick={() => table.previousPage()}
+        >
+          PREV PAGE
+        </button>
+        <button
+          disabled={!table.getCanNextPage()}
+          onClick={() => table.nextPage()}
+        >
+          NEXT PAGE
+        </button>
+        <button
+          disabled={!table.getCanNextPage()}
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+        >
+          LAST PAGE
+        </button>
       </div>
     </div>
   );
